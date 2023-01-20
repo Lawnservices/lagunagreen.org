83fbf73b6d14b86c6627e7951334cb3311d9c07d
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-01-20 09:41:25 -0500

    fix

--- contact.js
+++ contact.js
@@ -2,3 +2,7 @@ document.getElementById('print').addEventListener('click', function() {
     print.style = print();
     // print.style.display = 'none';
 });
+
+function laguna() {
+    alert('ready to print');
+}
